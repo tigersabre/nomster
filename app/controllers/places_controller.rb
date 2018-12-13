83041ec7d2093cc003b49313b725078a6c280dc1
Sarosh Arunkumar <sarosh.arunkumar@gmail.com>
2018-12-13 01:36:48 +0000

    Connect Users to Places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,7 @@
 class PlacesController < ApplicationController
   include Pagy::Backend
+
+  before_action :authenticate_user!, only: [:new, :create]
   def index
     @pagy, @places = pagy(Place.all)
   end
@@ -9,7 +11,7 @@ class PlacesController < ApplicationController
   end
 
   def create
-    Place.create(place_params)
+    current_user.places.create(place_params)
     redirect_to root_path
   end
 
