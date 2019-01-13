fcf130aeaa5c940c7cd95807822e53ea4af6323a
Sarosh Arunkumar <sarosh.arunkumar@gmail.com>
2019-01-13 19:03:05 +0000

    Adding a carousel

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,18 +5,19 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
 
-  # config.action_mailer.smtp_settings = {
-  #     address: "smtp.gmail.com",
-  #     port: 587,
-  #     domain: "gmail.com",
-  #     authentication: "plain",
-  #     enable_starttls_auto: "true",
-  #     user_name: ENV['GMAIL_ADDRESS'],
-  #     password: ENV['GMAIL_PASSWORD']
-  # }
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "gmail.com",
+      authentication: "plain",
+      enable_starttls_auto: "true",
+      user_name: ENV['GMAIL_ADDRESS'],
+      password: ENV['GMAIL_PASSWORD']
+  }
 
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'localhost:3030' }
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :smtp
 
 
   config.cache_classes = false
